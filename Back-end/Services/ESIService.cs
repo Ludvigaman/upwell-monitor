5395fbc7f1723c7bb403b50fb9db5e8c26352571
Ludvig Åman <lula@netcompany.com>
2025-01-09 15:16:18 +0100

    Added some testing stuff... That probably won't work.

--- Back-end/Services/ESIService.cs
+++ Back-end/Services/ESIService.cs
@@ -31,6 +31,8 @@ namespace Back_end.Services
                 4246  //Hyrdrogen Fuel Block
             };
 
+            var strontsId = 16275;
+
             var location_flag = "StructureFuel";
 
             // ------ Upwell structures ------
@@ -57,32 +59,39 @@ namespace Back_end.Services
             });
 
             // ------ POS bases ------
-            var starbaseResponse = await _client.Corporation.Starbases();
-            starbaseList.AddRange(starbaseResponse.Data);
+            //var starbaseIndexResponse = await _client.Corporation.Starbases();
+            //starbaseList.AddRange(starbaseIndexResponse.Data);
 
-            for (var i = 2; i <= starbaseResponse.Pages; i++)
-            {
-                var itemPage = await _client.Corporation.Starbases(i);
-                starbaseList.AddRange(itemPage.Data);
-            }
+            //for (var i = 2; i <= starbaseIndexResponse.Pages; i++)
+            //{
+            //    var itemPage = await _client.Corporation.Starbases(i);
+            //    starbaseList.AddRange(itemPage.Data);
+            //}
 
-            starbaseList.ForEach(starbase =>
-            {
-                var baseItem = _client.Corporation.Starbase(starbase.StarbaseId, starbase.SystemId);
+            //starbaseList.ForEach(async starbase =>
+            //{
+            //    var starbaseResponse = await _client.Corporation.Starbase(starbase.StarbaseId, starbase.SystemId);
+            //    var starbaseItem = starbaseResponse.Data;
 
-                //CONTINUE
+            //    //CONTINUE
 
-                //var struc = new StructureItem
-                //{
-                //    FuelBlocksInFuelBay = 0,
-                //    FuelExpires = null,
-                //    Id = baseItem.StructureId,
-                //    Name = starbase.Name,
-                //    TypeId = baseItem.TypeId
-                //};
-                //_returnStructures.Add(struc);
+            //    var fuelBlockLevel = 0;
+            //    foreach(var fuelType in starbaseItem.Fuels)
+            //    {
 
-            });
+            //    }
+
+            //    var struc = new StructureItem
+            //    {
+            //        FuelBlocksInFuelBay = 0,
+            //        FuelExpires = null,
+            //        Id = starbase.StarbaseId,
+            //        Name = starbase.Name,
+            //        TypeId = baseItem.TypeId
+            //    };
+            //    _returnStructures.Add(struc);
+
+            //});
 
             // ------ Assets ------
             var assetResponse = await _client.Assets.ForCorporation();
@@ -127,34 +136,6 @@ namespace Back_end.Services
                 structure.FuelBlocksPerDay = (int)Math.Round(consumption);
                 
             });
-
-
-
-            /* -------- Handle fuel reserves ------------ */
-            /* NOT WORKING, MEH
-
-            var fuelBlocksInReserves = _fuelBlocks.Where(item => item.LocationFlag != location_flag).ToList();
-
-            var fuelBlockReserveQuantities = fuelBlocksInReserves
-                .GroupBy(item => item.LocationId)
-                .Select(group => new
-                {
-                    LocationId = group.Key,
-                    TotalQuantity = group.Sum(item => item.Quantity)
-                })
-                .ToList();
-
-
-            _returnStructures.ForEach(structure =>
-            {
-              
-                var fuelBlocks = fuelBlockReserveQuantities.FirstOrDefault(fb => fb.LocationId == structure.id);
-
-              
-                structure.fuelBlocksInReserve = fuelBlocks?.TotalQuantity ?? 0;
-            });
-
-            */
 
             return _returnStructures;
 
